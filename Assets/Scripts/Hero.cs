6e3814429b266f9483d71eebe364134a14436d93
Oleh Hvozdetskyi <oleg.gvozdetskyi@gmail.com>
2021-12-12 14:18:59 +0200

    add early version of the game

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew
+{
+    public class Hero : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _jumpSpeed;
+
+        [SerializeField] private LayerCheck _groundCheck;
+
+        private Rigidbody2D _rigidbody;
+        private Vector2 _direction;
+        private float _jump;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public void SetDirection(Vector2 direction)
+        {
+            _direction = direction;
+        }
+
+        public void SetJump(float jump)
+        {
+            _jump = jump;
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+            var isJumping = _jump > 0;//_direction.y > 0
+            //check if you push up
+            if (_direction.y > 0)
+            {
+                Debug.Log("PUSHED UP");
+            }
+
+            if (isJumping)
+            {
+                if (IsGrounded())
+                {
+                    _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+                }
+            }
+
+            else if (_rigidbody.velocity.y > 0)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            return _groundCheck.IsTouchingLayer;
+        }
+
+        public void SaySomething()
+        {
+            Debug.Log("Hello!");
+        }
+    }
+}
---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PixelCrew
+{
+    public class HeroInputReader : MonoBehaviour
+    {
+        [SerializeField] private Hero _hero;
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            var direction = context.ReadValue<Vector2>();
+            _hero.SetDirection(direction);
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            var jump = context.ReadValue<float>();
+            _hero.SetJump(jump);
+        }
+
+        public void OnSaySomething(InputAction.CallbackContext context)
+        {
+            if (context.canceled)
+                _hero.SaySomething();
+        }
+    }
+}
