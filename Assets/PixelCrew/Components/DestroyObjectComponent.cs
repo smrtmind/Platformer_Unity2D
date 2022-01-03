5e63c9e211f87058c0409ddd626e49c17ccdc86e
Oleg Gvozdetskyi <oleg.gvozdetskyi@gmail.com>
2022-01-03 18:48:42 +0200

    create tilemaps, edit camera

---
+++ Assets/PixelCrew/Components/DestroyObjectComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.Components 
+{
+    public class DestroyObjectComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _objectToDestroy;
+
+        public void DestroyObject()
+        {
+            Destroy(_objectToDestroy);
+        }
+    }
+}
