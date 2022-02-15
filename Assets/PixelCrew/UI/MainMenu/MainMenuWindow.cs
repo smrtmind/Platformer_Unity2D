18af2273d302de62fe2650cdc7d6b385bb961bf8
Oleg Gvozdetskyi <oleg.gvozdetskyi@gmail.com>
2022-02-15 20:24:45 +0200

    add working starting menu with animations

---
+++ Assets/PixelCrew/UI/MainMenu/MainMenuWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PixelCrew.UI.MainMenu
+{
+    public class MainMenuWindow : WindowAnimation
+    {
+        private Action _closeAction;
+
+        public void OnStartGame()
+        {
+            _closeAction = () => { SceneManager.LoadScene("LVL_1"); };
+            Close();
+        }
+
+        public void OnShowOptions()
+        {
+            var window = Resources.Load<GameObject>("UI/OptionsWindow");
+            var canvas = FindObjectOfType<Canvas>();
+            Instantiate(window, canvas.transform);
+        }
+
+        public void OnLanguageOptions()
+        {
+            var window = Resources.Load<GameObject>("UI/LanguageWindow");
+            var canvas = FindObjectOfType<Canvas>();
+            Instantiate(window, canvas.transform);
+        }
+
+        public void OnExit()
+        {
+            _closeAction = () => 
+            {
+                Application.Quit();
+
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            };
+            Exit();
+        }
+
+        public override void OnCloseAnimationComplete()
+        {
+            _closeAction?.Invoke();
+            base.OnCloseAnimationComplete();
+        }
+    }
+}
---
+++ Assets/PixelCrew/UI/WindowAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.UI
+{
+    public class WindowAnimation : MonoBehaviour
+    {
+        private Animator _animator;
+        private static readonly int Show = Animator.StringToHash("show");
+        private static readonly int Hide = Animator.StringToHash("hide");
+        private static readonly int Escape = Animator.StringToHash("exit");
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+
+            _animator.SetTrigger(Show);
+        }
+
+        public void Close()
+        {
+            _animator.SetTrigger(Hide);
+        }
+
+        public void Exit()
+        {
+            _animator.SetTrigger(Escape);
+        }
+
+        public virtual void OnCloseAnimationComplete()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
