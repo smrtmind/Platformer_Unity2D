90aeaf87ff297816dc3182dcdbcd2ffcf240fbc7
Oleg Gvozdetskyi <oleg.gvozdetskyi@gmail.com>
2022-02-18 20:09:08 +0200

    fix critical bug with options menu, add fire effects

--- Assets/PixelCrew/Utils/WindowUtils.cs
+++ Assets/PixelCrew/Utils/WindowUtils.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace PixelCrew.Utils
 {
@@ -7,9 +8,19 @@ namespace PixelCrew.Utils
         public static void CreateWindow(string resourcePath)
         {
             var window = Resources.Load<GameObject>(resourcePath);
-            //var canvas = Object.FindObjectOfType<Canvas>();
-            var canvas = GameObject.FindWithTag("MainCanvas").GetComponent<Canvas>();
-            Object.Instantiate(window, canvas.transform);
+            var scene = SceneManager.GetActiveScene();
+            Canvas canvas;
+
+            if (scene.name == "MainMenu")
+            {
+                canvas = Object.FindObjectOfType<Canvas>();
+            }
+            else
+            {
+                canvas = GameObject.FindWithTag("MainCanvas").GetComponent<Canvas>();
+            }
+
+            Object.Instantiate(window, canvas.transform);         
         }
     }
 }
